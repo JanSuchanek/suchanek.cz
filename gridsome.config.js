1280c752c1dacd2c9865ffb998c5d2db27a77ff7
Jan Suchánek <jan.suchanek@gmail.com>
2019-10-16 11:39:27 +0200

    Oprava

--- gridsome.config.js
+++ gridsome.config.js
@@ -13,24 +13,6 @@ module.exports = {
         typeName: 'BlogPost',
         path: './content/blog/**/*.md',
       }
-    },
-    {
-      use: 'gridsome-source-wordpress',
-      options: {
-        baseUrl: 'https://www.hanyko-praha.cz', // required
-        apiBase: 'wp-json',
-        typeName: 'WordPress',
-        perPage: 100,
-        concurrent: 10,
-        routes: {
-          post: '/:year/:month/:day/:slug',
-          post_tag: '/tag/:slug'
-        },
-        splitPostsIntoFragments: true, // default false
-        downloadRemoteImagesFromPosts: true, // default false
-        downloadRemoteFeaturedImages: true, // default false
-        downloadACFImages: true, // default false
-      }
     }    
   ],
   templates: {
